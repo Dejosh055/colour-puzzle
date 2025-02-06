0666f6bc54b98df62a47c7c46cb3dfb1478c8e0f
Dejoah055 <Dejosh055@gmail.com>
2025-02-06 04:28:53 -1100

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+ 
+const gameStatus = document.querySelector('#status');
+const userScore = document.querySelector('#score');
+const newGame    = document.querySelector('#newgame')
+
+// function Hint(){
+//      gameStatus.textContent=`Guess the correct color! (Hint:${correctColor})`;
+// }
+function getRandomColor(){
+    const colors =['red','blue', 'green', 'yellow', 'orange' ,'purple']
+    return colors[Math.floor(Math.random()*colors.length)];
+}
+
+let correctColor = getRandomColor()
+let score = 0;
+function checkGuessColor( color,button){
+if(color === correctColor){
+score += 5;
+ gameStatus.textContent =`correct! the color was ${correctColor}.Generating a new color...`;
+ gameStatus.style.color = 'green';
+ button.classList.add("correct")
+ correctColor = getRandomColor();
+//  setTimeout(()=>{
+//     correctColor = getRandomColor();
+//     Hint()
+// //    gameStatus.textContent = `New color generated! (Hint:${correctColor})`;
+//    gameStatus.style.color = "black";
+//  }, 1000)
+
+}
+else{
+    score -= 5;
+    gameStatus.textContent = 'wrong guess! Try agin';
+    gameStatus.style.color = 'red'
+    button.classList.add("wrong") 
+
+
+}
+setTimeout(()=>{
+    button.classList.remove("correct","wrong")
+}, 500)
+userScore.textContent = `Score: ${score}`;
+}
+newGame.addEventListener('click',()=>{
+    score = 0
+    userScore.textContent = 'Score : 0'
+    gameStatus.textContent = ''
+
+  
+})
